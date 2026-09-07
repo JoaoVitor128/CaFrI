bef1599d49d09798ff2aede49235839439766464
João Vitor <157144910+JoaoVitor128@users.noreply.github.com>
2026-09-06 23:07:26 -0300

    Level 07 Route

---
+++ src/Routes/Level07.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Level07 = () => {
+  return (
+    <div>
+      <h1>Level 07</h1>
+      <div className="container ">
+
+      </div>
+    </div>
+  )
+}
+
+export default Level07
