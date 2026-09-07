5b64e9b5747d34650cd603c8aab42bf5aa3e847a
João Vitor <157144910+JoaoVitor128@users.noreply.github.com>
2026-09-07 11:14:21 -0300

    New Error Page

--- src/Routes/ErrorPage.jsx
+++ src/Routes/ErrorPage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 const ErrorPage = () => {
   return (
     <div className='container'>
-        <p>Errado.</p>
+        <h1>Errado!</h1>
+        <p>Mais sorte da próxima vez</p>
+        <p className='secondary'>
+          Good Luck.
+
+          3301
+        </p>
+        <Link to="/"><button>Voltar</button></Link>
     </div>
   )
 }
