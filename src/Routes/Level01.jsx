b277e6ce4ba76de500b070f6ac5e5e422ca2b4f4
João Vitor <157144910+JoaoVitor128@users.noreply.github.com>
2026-09-14 15:57:24 -0300

    Mudança de escopo das dicar e adição de título Level 01

--- src/Routes/Level01.jsx
+++ src/Routes/Level01.jsx
@@ -6,8 +6,9 @@ const Level01 = () => {
 
   return (
     <div>
+      <h1>Level 01 - Criação de</h1>
+      <Tips text={"Maior feito do indivíduo"} />
       <div className="container">
-        <Tips text={"Maior feito do indivíduo"}/>
         <p>... .- -. - --- ... / -.. ..- -- --- -. -</p>
         <p className='secondary'>A resposta deve ser colocar no link após a "/", por exeplo se você acha que a resposta é "Manhã Boa", você vai no LINK da página e escreva nesse modelo: https://cafri.netlify.app/manha-boa</p>
       </div>
