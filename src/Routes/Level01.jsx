0a8ffcb3dc668a3751d98bf95dc4a11637e099fe
João Vitor <157144910+JoaoVitor128@users.noreply.github.com>
2026-09-14 15:46:35 -0300

    Melhoramentoo Level 01 descrição de resposta

--- src/Routes/Level01.jsx
+++ src/Routes/Level01.jsx
@@ -7,9 +7,9 @@ const Level01 = () => {
   return (
     <div>
       <div className="container">
+        <Tips text={"Maior feito do indivíduo"}/>
         <p>... .- -. - --- ... / -.. ..- -- --- -. -</p>
-        <p className='secondary'>A resposta deve ser colocar no link após a "/", por exeplo se você acha que a resposta é "Manhã Boa", você vai no LINK da página e escreva https://cafri.netlify.app/manha-boa</p>
-        <p className='secondary'>A resposta deve ser: completamente minúscula, sem acentos e em cao de espaço(" "), adicione uma "-"</p>
+        <p className='secondary'>A resposta deve ser colocar no link após a "/", por exeplo se você acha que a resposta é "Manhã Boa", você vai no LINK da página e escreva nesse modelo: https://cafri.netlify.app/manha-boa</p>
       </div>
 
 
