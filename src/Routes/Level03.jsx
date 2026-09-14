0e23740e4401f0aee48fb9012f4585042b44bfa1
João Vitor <157144910+JoaoVitor128@users.noreply.github.com>
2026-09-14 13:51:55 -0300

    New Level 03

--- src/Routes/Level03.jsx
+++ src/Routes/Level03.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import { useRef } from 'react';
 import Tips from '../Components/Tips'
 
 import level03 from "../assets/level03.png"
+import nokia3310 from "../assets/nokia3310.mp3"
 
 const Level03 = () => {
+  const audioRef = useRef(new Audio(nokia3310));
+
+  const play = () => {
+    audioRef.current.play();
+  };
+
   return (
     <div>
-      <h1>Level 03 - Fonte</h1>
-      <Tips text="Inspecione-a" />
+      <h1>Level 03 - Tens uma nova mensagem</h1>
+      <Tips text="Deve ser horrível mandar mensagem assim" />
       <div className="container">
-        <img src={level03} alt="" className='level03' />
-        <p className="secondary">O que a por trás do CaFrI?</p>
+        <div>
+          <button onClick={play}>Tocar</button>
+        </div>
+        <p className="secondary">
+          1x2
+          2x6
+          3x6
+          1x6
+          1x2
+          3x5
+          3x4
+          1x2
+        </p>
       </div>
     </div>
   )
